6e5dfd1e813fba17f6f4034345635ad2e409df3d
furlopunk <furlopunk@gmail.com>
2018-01-26 13:33:50 -0500

    Initial Commit

---
+++ 99BottlesOfBeer/Form1.vb
@@ -0,0 +1,19 @@
+﻿Public Class Form1
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Me.Show()
+        BottlesSong()
+
+    End Sub
+
+    Private Sub BottlesSong()
+        Dim count As Integer = 99
+        While count > 0
+            RichTextBox1.AppendText(count.ToString + " bottles	of bee on the wall, " + count.ToString + " bottles of beer
+take one down, pass it around, " + (count - 1).ToString + " bottles	of beer	on the wall." + vbNewLine)
+            count -= 1
+        End While
+
+    End Sub
+
+
+End Class
