506edff4af6d45d58ed66a528ac89d56411aedf9
furlopunk <furlopunk@gmail.com>
2018-01-26 13:48:29 -0500

    Update Form1.vb

--- 99BottlesOfBeer/Form1.vb
+++ 99BottlesOfBeer/Form1.vb
@@ -1,10 +1,10 @@
 ﻿Public Class Form1
     Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
-        Me.Show()
+        Me.Show()'show form
         BottlesSong()
     End Sub
     Private Sub BottlesSong()
-     
+        'print 99 bottles of beer song in textbox.
     End Sub
 
 End Class
